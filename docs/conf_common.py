eb6f3653cb3a40808c9caf78da88d9366585ce7a
Krzysztof <krzysztof@espressif.com>
2026-08-07 16:40:40 +0800

    docs: Remove chatbot widget

--- docs/conf_common.py
+++ docs/conf_common.py
@@ -94,7 +94,6 @@ html_context['github_user'] = 'espressif'
 html_context['github_repo'] = 'esp-test-tools'
 
 html_static_path = ['../_static']
-html_css_files = ['js/chatbot_widget.css']
 
 # Extra options required by sphinx_idf_theme
 project_slug = 'esp-test-tools'
